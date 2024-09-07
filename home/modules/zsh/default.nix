f376130ac1143eb5c9fad58f7b3b1f5848b2390c
David Balatero <dbalatero@users.noreply.github.com>
2024-09-07 23:19:53 +0100

    whitespace

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -122,8 +122,6 @@ bindkey -M viins 'jk' vi-cmd-mode
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
-
-
     '';
   };
 }
