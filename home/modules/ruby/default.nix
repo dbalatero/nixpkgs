c688d9177b3fbcb715c03bda202eaa3343a0645c
David Balatero <dbalatero@users.noreply.github.com>
2024-09-08 21:27:32 +0100

    ruby-build

--- home/modules/ruby/default.nix
+++ home/modules/ruby/default.nix
@@ -1,7 +1,21 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.rbenv.enable = true;
+  programs.rbenv = {
+    enable = true;
+
+    plugins = [
+      {
+        name = "ruby-build";
+        src = pkgs.fetchFromGitHub {
+          owner = "rbenv";
+          repo = "ruby-build";
+          rev = "v20240903";
+          hash = "sha256-lhsdcTPVJkLURSz4yd952eGvReeVU10I9NxosMvKYSk=";
+        };
+      }
+    ];
+  };
 
   home.file.".pryrc".source = ./pryrc.rb;
 }
