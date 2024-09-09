28d0c145fc40f066413073fce5a792f80ce1c9cf
David Balatero <dbalatero@users.noreply.github.com>
2024-09-09 06:23:58 +0100

    more lsp

--- home/modules/neovim/plugins/lsp.nix
+++ home/modules/neovim/plugins/lsp.nix
@@ -1,13 +1,144 @@
 {
+  config,
   lib,
   pkgs,
   ...
-}: {
+}: let
+  helpers = config.lib.nixvim;
+in {
   home.packages = [pkgs.alejandra];
 
   programs.nixvim = {
+    keymaps = [
+      # Diagnostics
+      {
+        key = "<leader>e";
+        action = helpers.mkRaw "vim.diagnostic.open_float";
+        options = {
+          desc = "Open floating diagnostic message";
+        };
+      }
+      {
+        key = "<leader>q";
+        action = helpers.mkRaw "vim.diagnostic.setloclist";
+        options = {
+          desc = "Open diagnostics list";
+        };
+      }
+      {
+        key = "gk";
+        action =
+          helpers.mkRaw
+          # lua
+          ''
+            function()
+              vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+            end
+          '';
+        options = {
+          desc = "Go to previous error message";
+        };
+      }
+      {
+        key = "g<";
+        action = helpers.mkRaw "vim.diagnostic.goto_prev";
+        options = {
+          desc = "Go to previous diagnostic";
+        };
+      }
+      {
+        key = "gj";
+        action =
+          helpers.mkRaw
+          # lua
+          ''
+            function()
+              vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+            end
+          '';
+
+        options = {
+          desc = "Go to next error message";
+        };
+      }
+      {
+        key = "g>";
+        action = helpers.mkRaw "vim.diagnostic.goto_next";
+        options = {
+          desc = "Go to next diagnostic";
+        };
+      }
+
+      # Trouble
+      {
+        key = "<leader>xx";
+        action = "<cmd>Trouble diagnostics toggle<CR>";
+        options = {
+          silent = true;
+          remap = false;
+          desc = "Diagnostics (Trouble)";
+        };
+      }
+      {
+        key = "<leader>xb";
+        action = "<cmd>TroubleToggle diagnostics toggle filter.buf=0<CR>";
+        options = {
+          silent = true;
+          remap = false;
+          desc = "Buffer Diagnostics (Trouble)";
+        };
+      }
+      {
+        key = "<leader>xl";
+        action = "<cmd>Trouble loclist toggle<CR>";
+        options = {
+          silent = true;
+          remap = false;
+          desc = "Location List (Trouble)";
+        };
+      }
+      {
+        key = "<leader>xq";
+        action = "<cmd>Trouble qflist toggle<CR>";
+        options = {
+          silent = true;
+          remap = false;
+          desc = "Quickfix List (Trouble)";
+        };
+      }
+      {
+        key = "gR";
+        action = "<cmd>TroubleToggle lsp toggle focus=false win.position=right<CR>";
+        options = {
+          silent = true;
+          remap = false;
+          desc = "LSP Definitions / references / ... (Trouble)";
+        };
+      }
+    ];
+
     plugins = {
       lsp-format.enable = true;
+      web-devicons.enable = true;
+
+      fidget = {
+        enable = true;
+
+        progress = {
+          ignore = [
+            "none-ls"
+            "null-ls"
+          ];
+        };
+      };
+
+      trouble = {
+        enable = true;
+
+        settings = {
+          use_diagnostic_signs = true;
+        };
+      };
 
       lsp = {
         enable = true;
@@ -16,7 +147,54 @@
           bashls.enable = true;
           cssls.enable = true;
           html.enable = true;
-          lua-ls.enable = true;
+
+          jsonls = {
+            enable = true;
+            settings = {
+              json = {
+                schemas = [
+                  {
+                    description = "TypeScript compiler configuration file";
+                    fileMatch = ["tsconfig.json" "tsconfig.*.json"];
+                    url = "http://json.schemastore.org/tsconfig";
+                  }
+                  {
+                    description = "Babel configuration";
+                    fileMatch = [".babelrc.json" ".babelrc" "babel.config.json"];
+                    url = "http://json.schemastore.org/lerna";
+                  }
+                  {
+                    description = "ESLint config";
+                    fileMatch = [".eslintrc.json" ".eslintrc"];
+                    url = "http://json.schemastore.org/eslintrc";
+                  }
+                  {
+                    description = "Prettier config";
+                    fileMatch = [".prettierrc" ".prettierrc.json" "prettier.config.json"];
+                    url = "http://json.schemastore.org/prettierrc";
+                  }
+                  {
+                    description = "Vercel Now config";
+                    fileMatch = ["now.json"];
+                    url = "http://json.schemastore.org/now";
+                  }
+                  {
+                    description = "Stylelint config";
+                    fileMatch = [".stylelintrc" ".stylelintrc.json" "stylelint.config.json"];
+                    url = "http://json.schemastore.org/stylelintrc";
+                  }
+                ];
+              };
+            };
+          };
+
+          lua-ls = {
+            enable = true;
+            settings = {
+              telemetry.enable = false;
+              workspace.checkThirdParty = false;
+            };
+          };
 
           nil-ls = {
             enable = true;
@@ -26,6 +204,18 @@
           };
 
           tsserver.enable = true;
+          yamlls = {
+            enable = true;
+            settings = {
+              yaml = {
+                schemas = {
+                  # Github actions
+                  "https://json.schemastore.org/github-workflow" = ".github/workflows/*.{yml,yaml}";
+                  "https://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+                };
+              };
+            };
+          };
         };
       };
 
