1f5800001ff604eca19c0a3ec273914312656be6
David Balatero <dbalatero@users.noreply.github.com>
2024-09-10 23:33:50 -0400

    feat: add nix-darwin setup

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
