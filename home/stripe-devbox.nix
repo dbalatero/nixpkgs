836b8096fcc3c3bb3df875ab8f41422abe1d6025
David Balatero <dbalatero@users.noreply.github.com>
2024-10-24 09:44:33 -0400

    add stripe module to devbox

--- home/stripe-devbox.nix
+++ home/stripe-devbox.nix
@@ -1,12 +1,9 @@
 {
   imports = [
     ./modules/slim.nix
+    ./modules/stripe
   ];
 
   home.homeDirectory = "/home/dbalatero";
   home.username = "dbalatero";
-
-  # Use the default C-b on this Linux server, to avoid colliding when sshing to
-  # this machine.
-  programs.tmux.prefix = null;
 }
