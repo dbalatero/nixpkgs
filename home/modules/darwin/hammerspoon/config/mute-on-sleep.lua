a529bcf36db94d895445534cb0858596b032242b
David Balatero <dbalatero@users.noreply.github.com>
2024-11-13 11:12:24 -0500

    stop muting my desktop dac

--- home/modules/darwin/hammerspoon/config/mute-on-sleep.lua
+++ home/modules/darwin/hammerspoon/config/mute-on-sleep.lua
@@ -1,10 +1,32 @@
+local function isDeviceAllowlisted(device)
+  -- Which devices should we skip muting for?
+  local allowedDevices = {
+    "Schiit",
+  }
+
+  for _, allowedPattern in ipairs(allowedDevices) do
+    if device:name():match(allowedPattern) then
+      return true
+    end
+  end
+
+  return false
+end
+
 sleepWatcher = hs.caffeinate.watcher.new(function(state)
   if state == hs.caffeinate.watcher.systemDidWake then
     device = hs.audiodevice.defaultOutputDevice()
 
-    if device then
+    if not device then
+      return
+    end
+
+    if isDeviceAllowlisted(device) then
+      print("Skipping mute for allowlisted audio device: " .. device:name())
+    else
       device:setMuted(true)
-      hs.alert.show('Muted ' .. device:name())
+      print("Muted audio device: " .. device:name())
+      hs.alert.show("Muted " .. device:name())
     end
   end
 end)
