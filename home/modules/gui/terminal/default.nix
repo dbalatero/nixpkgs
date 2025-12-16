e31c7648bf0e24271aea8fd2ee6371562486389b
David Balatero <dbalatero@users.noreply.github.com>
2025-12-15 21:35:09 -0500

    feat: add emoji intensifies script

--- home/modules/gui/terminal/default.nix
+++ home/modules/gui/terminal/default.nix
@@ -1,9 +1,11 @@
-{
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./audio
     ./backups
     ./ghostty
+  ];
+
+  home.packages = with pkgs; [
+    (writeShellScriptBin "emoji-intensifies" (builtins.readFile ./bin/emoji-intensifies.sh))
   ];
 }
