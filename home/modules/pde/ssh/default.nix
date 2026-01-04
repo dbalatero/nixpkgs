7484d3c47d7772a81a092f388178419b66ebca60
David Balatero <dbalatero@users.noreply.github.com>
2026-01-04 16:26:54 -0500

    fix(ssh): ssh warning

--- home/modules/pde/ssh/default.nix
+++ home/modules/pde/ssh/default.nix
@@ -10,8 +10,12 @@
   programs.ssh = {
     enable = true;
 
-    # Automatically add keys to agent when first used
-    addKeysToAgent = "yes";
+    # Automatically add keys to agent when first used (for all hosts)
+    matchBlocks = {
+      "*" = {
+        addKeysToAgent = "yes";
+      };
+    };
 
     # Configure your GitHub SSH key (ignored if file doesn't exist)
     extraConfig = ''
