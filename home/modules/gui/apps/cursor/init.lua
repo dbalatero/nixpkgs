0df3810f73c3bc07ff7d86d275a0bc28db15b91b
David Balatero <dbalatero@users.noreply.github.com>
2026-01-03 20:12:39 -0500

    feat(cursor): quick open from cmd p

--- home/modules/gui/apps/cursor/init.lua
+++ home/modules/gui/apps/cursor/init.lua
@@ -1,1 +1,9 @@
+vim.opt.clipboard:append("unnamedplus")
 
+vim.keymap.set({ "n" }, "vv", function()
+  vim.cmd([[call VSCodeNotify('workbench.action.splitEditorRight')]])
+end)
+
+vim.keymap.set({ "n" }, "ss", function()
+  vim.cmd([[call VSCodeNotify('workbench.action.splitEditorDown')]])
+end)
