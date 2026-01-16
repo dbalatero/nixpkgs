c0ce2f6863fe464b4fda4a69d43371d898cd9ed8
David Balatero <dbalatero@users.noreply.github.com>
2026-01-16 11:58:36 -0500

    feat: jump to diagnostics

--- home/modules/gui/apps/cursor/init.lua
+++ home/modules/gui/apps/cursor/init.lua
@@ -1,9 +1,23 @@
 vim.opt.clipboard:append("unnamedplus")
 
+-- ╭─────────────────────────────────────────────────────────╮
+-- │ Fast vertical/horizontal splits                         │
+-- ╰─────────────────────────────────────────────────────────╯
 vim.keymap.set({ "n" }, "vv", function()
   vim.cmd([[call VSCodeNotify('workbench.action.splitEditorRight')]])
 end)
 
 vim.keymap.set({ "n" }, "ss", function()
   vim.cmd([[call VSCodeNotify('workbench.action.splitEditorDown')]])
+end)
+
+-- ╭─────────────────────────────────────────────────────────╮
+-- │ Jump to previous/next diagnostic errors                 │
+-- ╰─────────────────────────────────────────────────────────╯
+vim.keymap.set({ "n" }, "gj", function()
+  vim.cmd([[call VSCodeNotify('editor.action.marker.next')]])
+end)
+
+vim.keymap.set({ "n" }, "gk", function()
+  vim.cmd([[call VSCodeNotify('editor.action.marker.prev')]])
 end)
