39409aa0aa2e4d9c27d06c06be75dc32efb34833
David Balatero <dbalatero@users.noreply.github.com>
2026-01-17 14:14:54 -0500

    fix: only use id_github ssh key on github.com

--- home/modules/pde/ssh/default.nix
+++ home/modules/pde/ssh/default.nix
@@ -5,16 +5,14 @@
   programs.ssh = {
     enable = true;
 
-    # Automatically add keys to agent when first used (for all hosts)
     matchBlocks = {
       "*" = {
         addKeysToAgent = "yes";
       };
-    };
 
-    # Configure your GitHub SSH key (ignored if file doesn't exist)
-    extraConfig = ''
-      IdentityFile ~/.ssh/id_github
-    '';
+      "github.com" = {
+        identityFile = "~/.ssh/id_github";
+      };
+    };
   };
 }
