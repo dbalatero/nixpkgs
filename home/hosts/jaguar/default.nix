e18b55c17d16e251188584ddb802b81324e42d34
David Balatero <dbalatero@users.noreply.github.com>
2026-03-13 17:41:20 -0400

    update gt hook

--- home/hosts/jaguar/default.nix
+++ home/hosts/jaguar/default.nix
@@ -48,7 +48,6 @@
     export MONOLOGUE_SHELL_HOOKS_SKIP_MISE=1
 
     [ -f "$HOME/code/monologue/tools/vpn/bin/shell-integration.sh" ] && source "$HOME/code/monologue/tools/vpn/bin/shell-integration.sh"
-
-    eval "$($HOME/code/monologue/dev/shell-init zsh)" # monologue-shellrc
+    [ -f "$HOME/code/monologue/dev/shell-init" ] && eval "$("$HOME/code/monologue/dev/shell-init" zsh)"
   '';
 }
