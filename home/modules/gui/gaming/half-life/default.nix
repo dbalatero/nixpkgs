f98276ec4436805fa1d20d2a9b48a8dedfb4d7c3
David Balatero <dbalatero@users.noreply.github.com>
2026-05-02 12:14:10 -0400

    half-life cfg updates

--- home/modules/gui/gaming/half-life/default.nix
+++ home/modules/gui/gaming/half-life/default.nix
@@ -1,6 +1,6 @@
-{config, ...}: {
+{ config, ... }: {
   # Symlink Half-Life userconfig.cfg to Steam directory
   home.file.".local/share/Steam/steamapps/common/Half-Life/valve/userconfig.cfg" = {
-    source = ./userconfig.cfg;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nixpkgs/home/modules/gui/gaming/half-life/userconfig.cfg";
   };
 }
