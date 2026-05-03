ecd47d1a4aefa2f8856e5aa6190d70e193e2687e
David Balatero <dbalatero@users.noreply.github.com>
2026-05-03 10:45:28 -0400

    remove volumehud from older mac

--- darwin/hosts/lion/default.nix
+++ darwin/hosts/lion/default.nix
@@ -1,5 +1,11 @@
-{...}: {
+{lib, ...}: {
   imports = [
     ../common.nix
   ];
+
+  # Remove volumehud - requires macOS 26 (Tahoe)
+  homebrew.casks = lib.mkForce (
+    lib.filter (cask: cask != "volumehud")
+      (import ../common.nix {inherit lib;}).homebrew.casks
+  );
 }
