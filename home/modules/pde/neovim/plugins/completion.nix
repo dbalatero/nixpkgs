d34c13f72cc01067cd05ab74dc1ffdd5459ec1e9
David Balatero <dbalatero@users.noreply.github.com>
2026-05-21 14:29:54 -0400

    fix: blink completion keys

--- home/modules/pde/neovim/plugins/completion.nix
+++ home/modules/pde/neovim/plugins/completion.nix
@@ -23,14 +23,13 @@ in {
 
         settings = {
           keymap = {
-            preset = "super-tab";
-
-            # Disable super-tab's default Tab/Shift-Tab so custom handlers work
-            "<Tab>" = [];
-            "<S-Tab>" = [];
+            preset = "default";
 
             "<C-space>" = ["show" "show_documentation" "hide_documentation"];
             "<C-e>" = ["hide"];
+            "<CR>" = ["accept" "fallback"];
+            "<Tab>" = ["select_next" "snippet_forward" "fallback"];
+            "<S-Tab>" = ["select_prev" "snippet_backward" "fallback"];
             "<C-d>" = ["scroll_documentation_up" "fallback"];
             "<C-f>" = ["scroll_documentation_down" "fallback"];
           };
@@ -110,46 +109,6 @@ in {
       lua
       */
       ''
-        -- Custom Tab handler for blink.cmp
-        local function custom_tab()
-          local cmp = require("blink.cmp")
-
-          -- Handle snippet navigation first
-          if cmp.snippet_active() then
-            return cmp.snippet_forward()
-          end
-
-          -- If completion menu is visible, cycle through items
-          if cmp.is_visible() then
-            return cmp.select_next()
-          else
-            -- No menu, fallback to regular tab
-            return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-          end
-        end
-
-        -- Custom Shift-Tab handler for blink.cmp
-        local function custom_shift_tab()
-          local cmp = require("blink.cmp")
-
-          -- Handle snippet navigation first
-          if cmp.snippet_active() then
-            return cmp.snippet_backward()
-          end
-
-          -- If completion menu is visible, cycle backwards
-          if cmp.is_visible() then
-            return cmp.select_prev()
-          else
-            -- No menu, fallback to regular shift-tab
-            return vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
-          end
-        end
-
-        -- Set up custom Tab handlers
-        vim.keymap.set("i", "<Tab>", custom_tab, { desc = "Blink: Select next completion" })
-        vim.keymap.set("i", "<S-Tab>", custom_shift_tab, { desc = "Blink: Select prev completion" })
-
         -- Add custom snippets
         local ls = require("luasnip")
         ls.add_snippets("all", {
