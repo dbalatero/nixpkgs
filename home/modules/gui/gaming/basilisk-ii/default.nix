370d68ad0bfbd76f4da2de00120c2a8df14328bc
David Balatero <dbalatero@users.noreply.github.com>
2026-08-05 15:12:37 -0400

    macos updates

--- home/modules/gui/gaming/basilisk-ii/default.nix
+++ home/modules/gui/gaming/basilisk-ii/default.nix
@@ -9,9 +9,13 @@
   home.file.".basilisk_ii_prefs".text = ''
     rom /mnt/storage/Emulation/MacOS/ROMs/quadra-centris-610-650-800.rom
     # disk /mnt/storage/Emulation/MacOS/InstallMedia/mac-os-7.6-cd.image
+    disk */mnt/storage/Emulation/MacOS/InstallMedia/stuffit-expander-5.5-flat.img
     disk /mnt/storage/Emulation/MacOS/Disks/mac-os-7.6.hfv
     extfs /mnt/storage/Emulation/MacOS/Shared
-    screen win/1024/768
+    screen dga/1024/768
+    scale_integer true
+    scale_nearest true
+    init_grab true
     seriala /dev/null
     serialb /dev/null
     bootdrive 0
